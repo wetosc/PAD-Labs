1049342aad57d7087722eb2c760e03c599613dda
eugenius <eugen.cernei@gmail.com>
2017-12-20 20:18:56 +0200

    lab4: Added Round-Robin load balancing. Added a gitignore to the proxy go project.

--- lab3-4/code/proxy/main.go
+++ lab3-4/code/proxy/main.go
@@ -4,14 +4,38 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"sync"
 )
 
+type Index struct {
+	sync.Mutex
+	I   int32
+	Max int32
+}
+
+func (i *Index) Inc() int32 {
+	i.Lock()
+	defer i.Unlock()
+
+	i.I++
+	if i.I >= i.Max {
+		i.I = 0
+	}
+	return i.I
+}
+
+var i = Index{I: 0, Max: 2}
+
 func main() {
+	proxies := make([]*httputil.ReverseProxy, 0)
 	u, _ := url.Parse("http://localhost:3001")
-	proxy1 := httputil.NewSingleHostReverseProxy(u)
+	proxies = append(proxies, httputil.NewSingleHostReverseProxy(u))
+	u, _ = url.Parse("http://localhost:3002")
+	proxies = append(proxies, httputil.NewSingleHostReverseProxy(u))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		proxy1.ServeHTTP(w, r)
+
+		proxies[i.Inc()].ServeHTTP(w, r)
 	})
 	http.ListenAndServe(":3000", nil)
 }
