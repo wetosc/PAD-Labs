fc1fc9e5293e2099f9682cb99972b0b9dcbffae0
eugenius <eugen.cernei@gmail.com>
2017-12-21 00:20:47 +0200

    lab4: Added .rdb files to .gitignore . Changed the general build.sh to open all the services with one command.

--- lab3-4/code/build.sh
+++ lab3-4/code/build.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
+pm2 delete all
+pm2 start /opt/redis/redis-server 
 pm2 start dataNode/bin/www --name="node1" -- 3001
 pm2 start dataNode/bin/www --name="node2" -- 3002
+pm2 start proxy/proxy
